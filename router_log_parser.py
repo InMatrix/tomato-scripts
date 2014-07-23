e190893fd9fc7c5bf8e4a1a0be38cbebbf89ecf0
Tao Dong <tomdong@gmail.com>
2014-07-23 15:46:57 -0400

    Add home_id to MongoDB documents

--- router_log_parser.py
+++ router_log_parser.py
@@ -7,13 +7,14 @@ from datetime import datetime
 
 class RouterLogParser:
 
-    def __init__(self, home_id, db_conn_settings, db_name="home_trivia"):
+    def __init__(self, home_id, db_conn_settings, coll_access_log='net_access_log',
+                 coll_dev_list='dev_list', coll_prop='prop'):
         self.home_id = home_id
         self.client = MongoClient(**db_conn_settings)
-        self.db = self.client[db_name]
-        self.dev_list = self.db[home_id + "_dev_list"]
-        self.access_log = self.db[home_id + "_access_log"]
-        self.prop = self.db[home_id + "_prop"]
+        self.db = self.client.get_default_database()
+        self.dev_list = self.db[coll_dev_list]
+        self.access_log = self.db[coll_access_log]
+        self.prop = self.db[coll_prop]
 
     def update_dev_list(self, file_object, timestamp):
         """Update the device list collection in the database with the info in a new device list file received.
@@ -29,32 +30,31 @@ class RouterLogParser:
             dev_record = line.split()
             dev_name = dev_record[3]
             dev_ip = dev_record[2]
-            dev_rs = dev_list.find({'dev_name': dev_name})
-            if dev_rs.count() == 0:
-                # New device, add record
-                new_dev = {'dev_name': dev_name, 'ip': dev_ip, 'last_updated': my_date}
+            dev_rs = dev_list.find({'hid': self.home_id, 'dev_name': dev_name})
+
+            if dev_rs.count() == 0 or dev_ip != dev_rs.sort('last_updated', DESCENDING)[0]['ip']:
+                # If it's a new device or its ip has changed, then add a record.
+                new_dev = {'hid': self.home_id, 'dev_name': dev_name,
+                           'ip': dev_ip, 'last_updated': my_date}
                 dev_list.insert(new_dev)
-            else:
-                # Known device, then test if its IP has changed since last update.
-                last_ip = dev_rs.sort('last_updated', DESCENDING)[0]['ip']
-                if last_ip != dev_ip:
-                    new_dev = {'dev_name': dev_name, 'ip': dev_ip, 'last_updated': my_date}
-                    dev_list.insert(new_dev)  # Refactor
+            # else do nothing.
+
         # Update the timestamp of the most recently processed device list file
         self.update_dev_list_latest_updated(my_date)
         return True
 
     def update_dev_list_latest_updated(self, my_date):
         self.prop.update(
-            {'key': 'dev_list_latest_updated'},
-            {'key': 'dev_list_latest_updated', 'val': my_date},
+            {'hid': self.home_id, 'key': 'dev_list_latest_updated'},
+            {'hid': self.home_id, 'key': 'dev_list_latest_updated', 'val': my_date},
             upsert=True)
 
     def get_dev_list_latest_updated(self):
         prop = self.prop
         try:
-            dev_list_latest_updated = prop.find_one({'key': 'dev_list_latest_updated'})['val']
+            dev_list_latest_updated = prop.find_one({'hid': self.home_id, 'key': 'dev_list_latest_updated'})['val']
         except TypeError:
+            # Record does not exist
             dev_list_latest_updated = datetime.utcfromtimestamp(0)
         return dev_list_latest_updated
 
@@ -66,7 +66,7 @@ class RouterLogParser:
         4. If no record exists for this ip before the specified datetime, return nothing.
         """
         dev_list = self.dev_list
-        rs = dev_list.find({'ip': ip, 'last_updated': {'$lte': my_date}})
+        rs = dev_list.find({'hid': self.home_id, 'ip': ip, 'last_updated': {'$lte': my_date}})
         rs_ct = rs.count()
         if rs_ct == 0:
             return None
@@ -84,7 +84,8 @@ class RouterLogParser:
         access_log = self.access_log
         dev_list_latest_updated = self.get_dev_list_latest_updated()
         try:
-            latest_access_log_time = access_log.find_one(sort=[('accessed_on', DESCENDING)])['accessed_on']
+            latest_access_log_time = access_log.find_one({'hid': self.home_id},
+                                                         sort=[('accessed_on', DESCENDING)])['accessed_on']
         except TypeError:
             latest_access_log_time = datetime.utcfromtimestamp(0)
         # There can be more than one record updated with the same timestamp,
@@ -98,7 +99,7 @@ class RouterLogParser:
                 break
             ip = record[1]
             # Check if the timestamp is newer than the latest timestamp processed for the device list log.
-            my_doc = {'accessed_on': my_date}
+            my_doc = {'hid': self.home_id, 'accessed_on': my_date}
             if my_date <= dev_list_latest_updated:
                 dev_name = self.find_dev_name(ip, my_date)
                 if dev_name is not None:
@@ -114,11 +115,9 @@ class RouterLogParser:
          inserted before the device list has been updated."""
         access_log = self.access_log
         dev_list_latest_updated = self.get_dev_list_latest_updated()
-        rs = access_log.find(
-            {'accessed_on':
-                {'$lte': dev_list_latest_updated},
-                'ip': {'$exists': True}}
-        )
+        rs = access_log.find({'hid': self.home_id,
+                             'accessed_on': {'$lte': dev_list_latest_updated},
+                             'ip': {'$exists': True}})
         for r in rs:
             r['dev_name'] = self.find_dev_name(r['ip'], r['accessed_on'])
             r.pop('ip', None)
--- test_router_log_parser.py
+++ test_router_log_parser.py
@@ -5,11 +5,12 @@ from router_log_parser import RouterLogParser
 from pymongo import DESCENDING
 # import os, gzip
 from datetime import datetime
+import pdb
 
 
 @pytest.fixture(scope="class")
 def log_parser(request):
-    log_parser = RouterLogParser("h00", {}, db_name='test')
+    log_parser = RouterLogParser("h00", {"host": "mongodb://localhost/test"})
 
     def fin():
         log_parser.dev_list.drop()
@@ -28,9 +29,12 @@ class TestUpdateDevList():
         my_date = datetime.utcfromtimestamp(timestamp)
         with open(filename) as f:
             log_parser.update_dev_list(f, timestamp)
-        device = log_parser.dev_list.find_one({"dev_name": "Taos-iPhone-5S"})
-        count = log_parser.dev_list.find({'last_updated': my_date}).count()
-        dev_list_latest_updated = log_parser.prop.find_one({'key': 'dev_list_latest_updated'})['val']
+        device = log_parser.dev_list.find_one({'hid': log_parser.home_id, "dev_name": "Taos-iPhone-5S"})
+        count = log_parser.dev_list.find({'hid': log_parser.home_id, 'last_updated': my_date}).count()
+        dev_list_latest_updated = log_parser.prop.find_one(
+            {'hid': log_parser.home_id,
+             'key': 'dev_list_latest_updated'}
+        )['val']
         assert device['ip'] == "192.168.1.23" and device['last_updated'] == my_date \
             and count == 7 and dev_list_latest_updated == my_date
 
@@ -40,11 +44,18 @@ class TestUpdateDevList():
         my_date = datetime.utcfromtimestamp(timestamp)
         with open(filename) as f:
             log_parser.update_dev_list(f, timestamp)
-        tao_iphone = log_parser.dev_list.find_one({"dev_name": "Taos-iPhone-5S"}, sort=[('last_updated', DESCENDING)])
-        idea_pc = log_parser.dev_list.find_one({"dev_name": "idea-PC"})
-        latest_count = log_parser.dev_list.find({'last_updated': my_date}).count()
+        tao_iphone = log_parser.dev_list.find_one(
+            {'hid': log_parser.home_id,
+             "dev_name": "Taos-iPhone-5S"},
+            sort=[('last_updated', DESCENDING)]
+        )
+        idea_pc = log_parser.dev_list.find_one({'hid': log_parser.home_id, "dev_name": "idea-PC"})
+        latest_count = log_parser.dev_list.find({'hid': log_parser.home_id, 'last_updated': my_date}).count()
         total_count = log_parser.dev_list.find().count()
-        dev_list_latest_updated = log_parser.prop.find_one({'key': 'dev_list_latest_updated'})['val']
+        dev_list_latest_updated = log_parser.prop.find_one(
+            {'hid': log_parser.home_id,
+             'key': 'dev_list_latest_updated'}
+        )['val']
         assert tao_iphone['ip'] == "192.168.1.20" and tao_iphone['last_updated'] == my_date \
             and latest_count == 2 and total_count == 9 and idea_pc is not None \
             and dev_list_latest_updated == my_date
@@ -53,8 +64,8 @@ class TestUpdateDevList():
 class TestFindDevName():
 
     @pytest.fixture(scope="class")
-    def access_record(self):
-        return {"accessed_on": datetime.utcfromtimestamp(1397437700),
+    def access_record(self, log_parser):
+        return {'hid': log_parser.home_id, "accessed_on": datetime.utcfromtimestamp(1397437700),
                 'domain': 'www.facebook.com', 'ip': '192.168.1.20'}
 
     @pytest.fixture(scope="class")
@@ -65,10 +76,12 @@ class TestFindDevName():
     def init_dev_list(self, test_dev_name, log_parser):
         # setup the dev_list collection
         print("\nInserting a device list for test.")
-        my_devs = [{"dev_name": test_dev_name,
+        my_devs = [{'hid': log_parser.home_id,
+                    "dev_name": test_dev_name,
                     "ip": "192.168.1.20",
                     "last_updated": datetime.utcfromtimestamp(1397437705)},
-                   {"dev_name": "idea-PC",
+                   {'hid': log_parser.home_id,
+                    "dev_name": "idea-PC",
                     "ip": "192.168.1.28",
                     "last_updated": datetime.utcfromtimestamp(1397433316)}]
 
@@ -82,7 +95,8 @@ class TestFindDevName():
         """ If there is a single record matching the given IP with an earlier timestamp in the database,
         then get the dev_name from that record.
         """
-        new_dev = {"dev_name": test_dev_name,
+        new_dev = {'hid': log_parser.home_id,
+                   "dev_name": test_dev_name,
                    "ip": "192.168.1.20",
                    "last_updated": datetime.utcfromtimestamp(1397433316)}
         log_parser.dev_list.insert(new_dev)
@@ -93,7 +107,8 @@ class TestFindDevName():
         """ If there are more than one record matching the given IP with an earlier timestamp in the database,
         then get the dev_name from the latest record.
         """
-        new_dev = {"dev_name": 'idea-PC',
+        new_dev = {'hid': log_parser.home_id,
+                   "dev_name": 'idea-PC',
                    "ip": "192.168.1.20",
                    "last_updated": datetime.utcfromtimestamp(1397435316)}
         log_parser.dev_list.insert(new_dev)
@@ -111,26 +126,33 @@ class TestWriteAccessLog():
 
     def test_process_delayed_access_records(self, log_parser, reset_collections):
         # Prepare test data
-        access_records = [{"dev_name": "idea-PC",
+        access_records = [{'hid': log_parser.home_id,
+                           "dev_name": "idea-PC",
                            "domain": "www.google.com",
                            "accessed_on": datetime.utcfromtimestamp(1397433316)},
-                          {"dev_name": "idea-PC",
+                          {'hid': log_parser.home_id,
+                           "dev_name": "idea-PC",
                            "domain": "www.facebook.com",
                            "accessed_on": datetime.utcfromtimestamp(1397433316)},
-                          {"ip": "192.168.1.20",
+                          {'hid': log_parser.home_id,
+                           "ip": "192.168.1.20",
                            "domain": "www.twitter.com",
                            "accessed_on": datetime.utcfromtimestamp(1397437705)},
-                          {"ip": "192.168.1.20",
+                          {'hid': log_parser.home_id,
+                           "ip": "192.168.1.20",
                            "domain": "www.twitter.com",
                            "accessed_on": datetime.utcfromtimestamp(1397437710)},
-                          {"ip": "192.168.1.20",
+                          {'hid': log_parser.home_id,
+                           "ip": "192.168.1.20",
                            "domain": "www.twitter.com",
                            "accessed_on": datetime.utcfromtimestamp(1397437805)}]
 
-        dev_list = [{"dev_name": 'iPhone-5S',
+        dev_list = [{'hid': log_parser.home_id,
+                     "dev_name": 'iPhone-5S',
                      "ip": "192.168.1.20",
                      "last_updated": datetime.utcfromtimestamp(1397437705)},
-                    {"dev_name": "idea-PC",
+                    {'hid': log_parser.home_id,
+                     "dev_name": "idea-PC",
                      "ip": "192.168.1.28",
                      "last_updated": datetime.utcfromtimestamp(1397433316)}]
 
@@ -140,9 +162,9 @@ class TestWriteAccessLog():
         log_parser.update_dev_list_latest_updated(latest_dev_list_time)
 
         log_parser.process_delayed_access_records()
-        r1 = log_parser.access_log.find_one({"accessed_on": datetime.utcfromtimestamp(1397437705)})
-        r2 = log_parser.access_log.find_one({"accessed_on": datetime.utcfromtimestamp(1397437710)})
-        r3 = log_parser.access_log.find_one({"accessed_on": datetime.utcfromtimestamp(1397437805)})
+        r1 = log_parser.access_log.find_one({'hid': log_parser.home_id, "accessed_on": datetime.utcfromtimestamp(1397437705)})
+        r2 = log_parser.access_log.find_one({'hid': log_parser.home_id, "accessed_on": datetime.utcfromtimestamp(1397437710)})
+        r3 = log_parser.access_log.find_one({'hid': log_parser.home_id, "accessed_on": datetime.utcfromtimestamp(1397437805)})
         assert r1['dev_name'] == 'iPhone-5S' and r2['dev_name'] == 'iPhone-5S' \
             and 'ip' not in r1 and 'ip' not in r2 \
             and 'dev_name' not in r3 and r3['ip'] == "192.168.1.20"
@@ -154,22 +176,27 @@ class TestWriteAccessLog():
         """
         access_log = log_parser.access_log
         # Prepare test data
-        dev_list = [{"dev_name": 'MacBook-Air',
+        dev_list = [{'hid': log_parser.home_id, 
+                     "dev_name": 'MacBook-Air',
                      "ip": "192.168.1.40",
                      "last_updated": datetime.utcfromtimestamp(1397415116)},
-                    {"dev_name": "iPhone-5S",
+                    {'hid': log_parser.home_id,
+                     "dev_name": "iPhone-5S",
                      "ip": "192.168.1.23",
                      "last_updated": datetime.utcfromtimestamp(1397415116)},
-                    {"dev_name": "idea-PC",
+                    {'hid': log_parser.home_id,
+                     "dev_name": "idea-PC",
                      "ip": "192.168.1.49",
                      "last_updated": datetime.utcfromtimestamp(1397415116)}]
         log_parser.dev_list.insert(dev_list)
 
         access_records = [
-            {"dev_name": "MacBook-Air",
+            {'hid': log_parser.home_id,
+             "dev_name": "MacBook-Air",
              "domain": "hmma.baidu.com",
              "accessed_on": datetime.utcfromtimestamp(1397415483)},
-            {"dev_name": "MacBook-Air",
+            {'hid': log_parser.home_id,
+             "dev_name": "MacBook-Air",
              "domain": "www.google.com",
              "accessed_on": datetime.utcfromtimestamp(1397415483)}
         ]
@@ -182,8 +209,12 @@ class TestWriteAccessLog():
         with open('testdata/webmon_recent_domains_test') as f:
             log_parser.write_access_log(f)
 
-        records_ct = access_log.find().count()
-        records_delay_ct = access_log.find({'dev_name': {'$exists': False}, 'ip': {'$exists': True}}).count()
+        records_ct = access_log.find({'hid': log_parser.home_id}).count()
+        records_delay_ct = access_log.find(
+            {'hid': log_parser.home_id,
+             'dev_name': {'$exists': False},
+             'ip': {'$exists': True}}
+        ).count()
 
         assert records_ct == 11 and records_delay_ct == 4
 
